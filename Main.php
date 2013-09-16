624eea123c88886991cdccc01bf34b4b2ee15985
Marcus Povey <marcus@marcus-povey.co.uk>
2013-09-16 12:40:13 +0100

    First outline.

--- Main.php
+++ Main.php
@@ -4,6 +4,20 @@
 
         class Main extends \Idno\Common\Plugin {
 
+            /**
+             * Find a user based on the secret email address they can use to post to.
+             * @param type $email
+             * @return boolean
+             */
+            static function getUserBySecretEmail($email) {
+                if ($result = \Idno\Core\site()->db()->getObjects('Idno\\Entities\\User', array('secret_email' => $email), null, 1)) {
+                    foreach ($result as $row) {
+                        return $row;
+                    }
+                }
+                return false;
+            }
+            
             function registerPages() {
 	    }
 
--- script/incoming_email.php
+++ script/incoming_email.php
@@ -1,3 +1,53 @@
 <?php
 
 	require_once(dirname(dirname(dirname(__FILE__))) . '/Idno/start.php');
+
+	$EmailParser = new MimeMailParser();
+
+    	$stream = fopen("php://stdin", "r");
+    	$EmailParser->setStream($stream);
+
+	// Extract some basics
+    	$to = $EmailParser->getHeader('to');
+    	$from = $EmailParser->extractEmail('from');
+    	$subject = $EmailParser->getHeader('subject');
+
+        // See if we've got a user with that email
+        if ($user = \IdnoPlugins\IdnoEmailPosting\Main::getUserBySecretEmail($to)) {
+            
+            // Log user on
+            $session = \Idno\Core\site()->session;
+            $session->logUserOn($user);
+            
+            // Get body
+            $text = $EmailParser->getMessageBody('text');
+            $html = $EmailParser->getMessageBody('html');
+
+            // Get Any attachments
+            $attachments = $EmailParser->getAttachments();
+
+            // Get message body
+            $message_body = $text;
+
+            // Santise outlook and outlook web
+            list ($message_body) = explode("From: ". \Idno\Core\site()->config()->title." [mailto:", $message_body);
+            list ($message_body) = explode("________________________________", $message_body);
+            
+            // Eliminate a possible security hole where the special email address could be printed in the body (from jettmail)
+            $message_body = str_replace($EmailParser->extractEmail('to'), "", $message_body);
+
+            // Trigger post handlers
+            \Idno\Core\site()->triggerEvent('email/post', [
+                'from' => $from,
+                'to' => $to,
+                'subject' => $subject,
+                'body' => $message_body,
+                'attachments' => $attachments,
+                'user' => $user
+            ]);
+            
+        }
+
+        // Done, log out
+        $session = \Idno\Core\site()->session;
+        $session->logUserOff();
